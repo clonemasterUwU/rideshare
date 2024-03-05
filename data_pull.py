7c10f71efebb0e0cf30bc94c5aaa39a20c625b72
clonemasteruwu <huskar.the.scared.warrior@gmail.com>
2024-03-04 21:22:41 -0500

    update data pull script to download and extract census tractsOC and community areas

--- data_pull.py
+++ data_pull.py
@@ -1,21 +1,41 @@
-import os, sys, requests, shutil
+import os, sys, requests, zipfile
 from multiprocessing import Pool, cpu_count
 
 
 def download(file_name_and_url):
     file_name, url = file_name_and_url
-    if os.path.exists(file_name):
-        print("{} already exists".format(file_name))
-        return
-    try:
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(file_name, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=16392):
-                    f.write(chunk)
-        print("finish downloading {}".format(file_name))
-    except Exception as e:
-        print("error downloading {}: {}".format(file_name, e))
+    if file_name.endswith('.zip'):
+        folder_name = file_name[:-4]
+        if os.path.exists(folder_name):
+            print("{} already exists".format(folder_name))
+            return
+        os.makedirs(folder_name)
+        try:
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                with open(file_name, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=16392):
+                        f.write(chunk)
+            print("finish downloading {}".format(file_name))
+            with zipfile.ZipFile(file_name) as z:
+                z.extractall(folder_name)
+            os.remove(file_name)
+            print("finish extracting {}".format(file_name))
+        except Exception as e:
+            print("error downloading {}: {}".format(file_name, e))
+    else:
+        if os.path.exists(file_name):
+            print("{} already exists".format(file_name))
+            return
+        try:
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                with open(file_name, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=16392):
+                        f.write(chunk)
+            print("finish downloading {}".format(file_name))
+        except Exception as e:
+            print("error downloading {}: {}".format(file_name, e))
 
 
 if __name__ == "__main__":
@@ -27,7 +47,11 @@ if __name__ == "__main__":
             2023: "e55j-2ewb"}
     # urls = ["https://data.cityofchicago.org/api/views/{}/rows.csv?accessType=DOWNLOAD&api_foundry=true".format(x)]
     arg = [(os.path.join(data_dir_path, "Chicago.osm.pbf"),
-            "https://download.bbbike.org/osm/bbbike/Chicago/Chicago.osm.pbf")]
+            "https://download.bbbike.org/osm/bbbike/Chicago/Chicago.osm.pbf"),
+           (os.path.join(data_dir_path, "community_area.zip"),
+            "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=Shapefile"),
+           (os.path.join(data_dir_path, "census_tract_2010.zip"),
+            "https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=Shapefile")]
     for year in sys.argv[1:]:
         try:
             if int(year) in maps:
@@ -40,7 +64,7 @@ if __name__ == "__main__":
             print("int argument, not {}".format(year))
     # print(sys.argv)
     # print("There are {} CPUs on this machine ".format(cpu_count()))
-    pool = Pool(cpu_count())
+    pool = Pool(min(cpu_count(), len(arg)))
     results = pool.map(download, arg)
     pool.close()
     pool.join()
