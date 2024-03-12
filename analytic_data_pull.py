57293facd52d4a4cdbce38d1fb819a3ec8c81530
clonemasteruwu <huskar.the.scared.warrior@gmail.com>
2024-03-11 21:11:55 -0400

    script update

--- analytic_data_pull.py
+++ analytic_data_pull.py
@@ -1,68 +1,74 @@
-import os, sys, requests, zipfile
+import argparse
+import csv
+import datetime
+import os
+import requests
+import tempfile
+import zipfile
+import io
+from datetime import datetime
 from multiprocessing import Pool, cpu_count
+from pathlib import Path
 
 
-def download(file_name_and_url):
-    file_name, url = file_name_and_url
-    if file_name.endswith('.zip'):
-        folder_name = file_name[:-4]
-        if os.path.exists(folder_name):
-            print("{} already exists".format(folder_name))
-            return
-        os.makedirs(folder_name)
-        try:
+def download(parameters):
+    directory, file_name, extension, url = parameters
+    os.makedirs(directory, exist_ok=True)
+    try:
+        with open(file_name, 'wb') if extension == 'osm' else tempfile.TemporaryFile() as temp_file:
             with requests.get(url, stream=True) as r:
                 r.raise_for_status()
-                with open(file_name, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=16392):
-                        f.write(chunk)
+                for chunk in r.iter_content(chunk_size=16392):
+                    temp_file.write(chunk)
             print("finish downloading {}".format(file_name))
-            with zipfile.ZipFile(file_name) as z:
-                z.extractall(folder_name)
-            os.remove(file_name)
+            if extension == 'zip':
+                with zipfile.ZipFile(temp_file) as z:
+                    z.extractall(directory / file_name)
+            elif extension == 'csv':
+                temp_file.seek(0)
+                date_format = "%m/%d/%Y %I:%M:%S %p"
+                with io.TextIOWrapper(temp_file) as in_file, open(directory / file_name, "w", newline='') as out_file:
+                    reader = csv.reader(in_file)
+                    header = next(reader)
+                    data = sorted(reader, key=lambda x: datetime.strptime(x[2], date_format))
+                    writer = csv.writer(out_file)
+                    writer.writerow(header)
+                    writer.writerows(data)
             print("finish extracting {}".format(file_name))
-        except Exception as e:
-            print("error downloading {}: {}".format(file_name, e))
-    else:
-        if os.path.exists(file_name):
-            print("{} already exists".format(file_name))
-            return
-        try:
-            with requests.get(url, stream=True) as r:
-                r.raise_for_status()
-                with open(file_name, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=16392):
-                        f.write(chunk)
-            print("finish downloading {}".format(file_name))
-        except Exception as e:
-            print("error downloading {}: {}".format(file_name, e))
+    except Exception as e:
+        print("error trying {}: {}".format(file_name, e))
 
 
 if __name__ == "__main__":
-    data_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
-    if not os.path.exists(data_dir_path):
-        os.makedirs(data_dir_path)
+    parser = argparse.ArgumentParser(description="Pull data for analytic")
+    parser.add_argument("--dir", required=True, help="Data directory")
+    parser.add_argument("--years", nargs="*", type=int, help="List of years to pull csv data",
+                        choices=range(2013, 2024))
+    args = parser.parse_args()
+    print(args)
+    data_dir = Path(args.dir)
+    if not os.path.exists(data_dir):
+        print("{} does not exist or permission is not granted. Try to create directory...".format(data_dir))
+        os.makedirs(data_dir)
+        print("directory created!")
+
     maps = {2013: "6h2x-drp2", 2014: "66as-63gf", 2015: "9arg-bn2i", 2016: "bk5j-9eu2", 2017: "jeij-fq8w",
             2018: "vbsw-zws8", 2019: "h4cq-z3dy", 2020: "r2u4-wwk3", 2021: "9kgb-ykyt", 2022: "npd7-ywjz",
             2023: "e55j-2ewb"}
-    # urls = ["https://data.cityofchicago.org/api/views/{}/rows.csv?accessType=DOWNLOAD&api_foundry=true".format(x)]
-    arg = [(os.path.join(data_dir_path, "Chicago.osm.pbf"),
-            "https://download.bbbike.org/osm/bbbike/Chicago/Chicago.osm.pbf"),
-           (os.path.join(data_dir_path, "community_area.zip"),
-            "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=Shapefile"),
-           (os.path.join(data_dir_path, "census_tract_2010.zip"),
-            "https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=Shapefile")]
-    for year in sys.argv[1:]:
-        try:
-            if int(year) in maps:
-                arg.append((os.path.join(data_dir_path, "{}.csv".format(year)),
-                            "https://data.cityofchicago.org/api/views/{}/rows.csv?accessType=DOWNLOAD&api_foundry=true"
-                            .format(maps[int(year)])))
-            else:
-                print("year {} is not in data list".format(year))
-        except ValueError:
-            print("int argument, not {}".format(year))
-    pool = Pool(min(cpu_count(), len(arg)))
-    results = pool.map(download, arg)
+    # # urls = ["https://data.cityofchicago.org/api/views/{}/rows.csv?accessType=DOWNLOAD&api_foundry=true".format(x)]
+    arguments = [(data_dir / "osm", "Chicago.osm.pbf", "pbf",
+                  "https://download.bbbike.org/osm/bbbike/Chicago/Chicago.osm.pbf"),
+                 (data_dir, "community_area", "zip",
+                  "https://data.cityofchicago.org/api/geospatial/cauq-8yn6?method=export&format=Shapefile"),
+                 (data_dir, "census_tract", "zip",
+                  "https://data.cityofchicago.org/api/geospatial/5jrd-6zik?method=export&format=Shapefile")]
+    for year in args.years:
+        arguments.append((data_dir / "chicago_taxi_csv", str(year) + ".csv", "csv",
+                          "https://data.cityofchicago.org/api/views/{}/rows.csv?accessType=DOWNLOAD&api_foundry=true"
+                          .format(maps[int(year)])))
+    arguments = list(filter(lambda x: not os.path.exists(x[0] / x[1]), arguments))
+    print(arguments)
+    pool = Pool(min(cpu_count(), len(arguments)))
+    results = pool.map(download, arguments)
     pool.close()
     pool.join()
